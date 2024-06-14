ac74fe18656f64a8ff66fe290817b77fd36e30b8
starwizard2005 <achyuth.hw@gmail.com>
2024-06-14 23:51:35 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <functional>
+
+using namespace std;
+
+int main() {
+    int n,q;
+    cin >> n >> q;
+    int sum = 0;
+    priority_queue<int> maxx;
+    priority_queue<int, vector<int>, greater<int>> minn;
+    for (int i = 0;i < n;i++){
+        int a;
+        cin >> a;
+        maxx.push(a);
+        minn.push(a);
+        sum+=a;
+    }
+    int mx = -1;
+    int queries[q];
+    for (int i = 0;i<q;i++){
+        cin >> queries[i];
+        if (queries[i]>mx){
+            mx = queries[i];
+        }
+    }
+    vector <int> answers;
+    answers.push_back(sum);
+    for (int i = 0;i < mx;i++){
+        int p = maxx.top();
+        int q = minn.top();
+        minn.pop();
+        maxx.pop();
+        sum-=p;
+        sum-=q;
+        int el =  p-q;
+        maxx.push(el);
+        minn.push(el);
+        sum+=el;
+        answers.push_back(sum);
+    }
+    for (int i = 0;i<q;i++){
+        cout << answers[queries[i]] << " ";
+    }
+
+    return 0;
+}
